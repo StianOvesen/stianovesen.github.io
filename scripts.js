d486cf14d59ff2f25c54269654232910134777d2
Satyam Sagar <satyamsagar1711@gmail.com>
2022-10-30 01:12:23 +0530

    added darkmode status to local storage

--- scripts.js
+++ scripts.js
@@ -3,5 +3,16 @@ function darkMode() {
     body.classList.toggle("dark-mode");
     
     document.getElementById("heading").classList.toggle("to-home-dark");
-    document.getElementById("navbar").classList.toggle("navbar-dark");   
+    document.getElementById("navbar").classList.toggle("navbar-dark");
+    if(body.classList.contains("dark-mode")){
+        localStorage.setItem("dark-mode", "true");
+    } else {
+        localStorage.setItem("dark-mode", "false");
+    }
 }
+document.addEventListener("DOMContentLoaded", function() {
+    if (localStorage.getItem("dark-mode") === "true") {
+        darkMode();
+    }
+    document.getElementById("btn-darkmode").addEventListener("click", darkMode);
+});
