eb9e78e11aa07225fc96d9af5e01af556da7aaf0
StianOvesen <stianovesen@me.com>
2022-10-29 21:52:56 +0200

    Revert "added darkmode status to local storage"

--- scripts.js
+++ scripts.js
@@ -3,16 +3,5 @@ function darkMode() {
     body.classList.toggle("dark-mode");
     
     document.getElementById("heading").classList.toggle("to-home-dark");
-    document.getElementById("navbar").classList.toggle("navbar-dark");
-    if(body.classList.contains("dark-mode")){
-        localStorage.setItem("dark-mode", "true");
-    } else {
-        localStorage.setItem("dark-mode", "false");
-    }
+    document.getElementById("navbar").classList.toggle("navbar-dark");   
 }
-document.addEventListener("DOMContentLoaded", function() {
-    if (localStorage.getItem("dark-mode") === "true") {
-        darkMode();
-    }
-    document.getElementById("btn-darkmode").addEventListener("click", darkMode);
-});
